5a7c152ea3abfc953a3bf4ffeef1605664a0cd52
Wen Xiaotong <1525528992@qq.com>
2023-08-06 14:42:52 +0800

    HW1 is finished

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -77,6 +77,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
+        if(isEmpty()){
+            throw new RuntimeException("Ring buffer underflow");
+        }
         return rb[first];
         // TODO: Return the first item. None of your instance variables should change.
     }
